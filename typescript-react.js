d1161c000e74dfc1229766e1f18aeab5d1712b09
Nathan Hardy <nhardy@live.com.au>
2018-09-16 13:47:50 +1000

    Disable react/sort-comp for typescript-react

--- typescript-react.js
+++ typescript-react.js
@@ -4,5 +4,7 @@ module.exports = Object.assign({}, base, {
   extends: require.resolve('eslint-config-airbnb'),
   rules: Object.assign({}, base.rules, {
     'react/jsx-filename-extension': ['error', { extensions: ['.jsx', '.tsx'] }],
+    // Conflicts with typescript/member-ordering
+    'react/sort-comp': 'off',
   }),
 });
