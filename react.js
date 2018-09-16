8c080cdc24195d97b4eb0aa92a3f3e7dc3f2b7f7
Nathan Hardy <nhardy@live.com.au>
2018-09-16 14:03:31 +1000

    Override eslint-config-airbnb default for jsx-a11y requiring both nesting and id for label

--- react.js
+++ react.js
@@ -5,5 +5,13 @@ module.exports = Object.assign({}, base, {
   rules: {
     // This rule causes issues with asynchronous code
     'react/destructuring-assignment': 'off',
+    // Override eslint-config-airbnb default of requiring both nesting and id
+    'jsx-a11y/label-has-for': ['error', {
+      components: [],
+      required: {
+        some: ['nesting', 'id'],
+      },
+      allowChildren: false,
+    }],
   },
 });
