e2c662133ad9ce14812a414b83b45354a919685f
Nathan Hardy <nhardy@live.com.au>
2018-09-16 13:55:40 +1000

    Disable react/destructuring-assignment

--- react.js
+++ react.js
@@ -2,4 +2,8 @@ const base = require('.');
 
 module.exports = Object.assign({}, base, {
   extends: require.resolve('eslint-config-airbnb'),
+  rules: {
+    // This rule causes issues with asynchronous code
+    'react/destructuring-assignment': 'off',
+  },
 });
--- typescript-react.js
+++ typescript-react.js
@@ -1,8 +1,9 @@
 const base = require('./typescript');
+const react = require('./react');
 
 module.exports = Object.assign({}, base, {
   extends: require.resolve('eslint-config-airbnb'),
-  rules: Object.assign({}, base.rules, {
+  rules: Object.assign({}, base.rules, react.rules, {
     'react/jsx-filename-extension': ['error', { extensions: ['.jsx', '.tsx'] }],
     // Conflicts with typescript/member-ordering
     'react/sort-comp': 'off',
