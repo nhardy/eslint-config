633385e5eea5fa5cf1f9255a48739eb9b62e6c4d
Nathan Hardy <nhardy@live.com.au>
2019-03-06 11:31:49 +1100

    Require only that label has either nested element OR for

--- rules/react.js
+++ rules/react.js
@@ -2,6 +2,10 @@ module.exports = {
   // This rule causes issues with asynchronous code
   'react/destructuring-assignment': 'off',
   // Override eslint-config-airbnb default of requiring both nesting and id
+  'jsx-a11y/label-has-associated-control': [
+    'error',
+    { assert: 'either' },
+  ],
   'jsx-a11y/label-has-for': ['error', {
     components: [],
     required: {
